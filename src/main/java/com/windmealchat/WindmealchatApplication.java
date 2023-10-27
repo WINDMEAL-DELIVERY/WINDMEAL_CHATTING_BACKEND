21a69ff05563492a32689803081867e8e74cc464
Owen-Choi <oldstyle482@gmail.com>
2023-10-27 16:34:46 +0900

    Initial Commit

---
+++ src/main/java/com/windmealchat/WindmealchatApplication.java
@@ -0,0 +1,13 @@
+package com.windmealchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindmealchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindmealchatApplication.class, args);
+    }
+
+}
