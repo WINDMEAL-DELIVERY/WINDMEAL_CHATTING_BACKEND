c37d552085a4c57e4be65381902c7db9a8283f58
Owen-Choi <oldstyle482@gmail.com>
2024-01-16 23:12:17 +0900

    Refactor : 예외 메시지 커스텀

--- src/main/java/com/windmealchat/global/handler/HttpHandShakeInterceptor.java
+++ src/main/java/com/windmealchat/global/handler/HttpHandShakeInterceptor.java
@@ -6,19 +6,28 @@ import static com.windmealchat.global.constants.TokenConstants.CODE_A;
 import static com.windmealchat.global.constants.TokenConstants.PREFIX_REFRESHTOKEN;
 import static com.windmealchat.global.constants.TokenConstants.TOKEN;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.windmealchat.chat.exception.AuthorizationException;
+import com.windmealchat.global.exception.ErrorCode;
+import com.windmealchat.global.exception.ExceptionResponseDTO;
 import com.windmealchat.global.token.dao.RefreshTokenDAO;
 import com.windmealchat.global.token.impl.TokenProvider;
 import com.windmealchat.global.util.AES256Util;
 import com.windmealchat.member.dto.response.MemberInfoDTO;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeFailureException;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 @Slf4j
@@ -26,36 +35,28 @@ import org.springframework.web.socket.server.HandshakeInterceptor;
 public class HttpHandShakeInterceptor implements HandshakeInterceptor {
 
   private final AES256Util aes256Util;
+  private final ObjectMapper objectMapper;
   private final TokenProvider tokenProvider;
   private final RefreshTokenDAO refreshTokenDAO;
 
-  /*
-      ServletHttpRequest에서 쿠키를 가져온 다음, attributes에 저장해준다.
-      그리고 해당 값을 인바운드 채널 인터셉터에서 시큐리티의 필터처럼 검증해주면 된다.
-   */
   @Override
   public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
-      WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+      WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception{
     if (request instanceof ServletServerHttpRequest) {
       ServletServerHttpRequest servletServerHttpRequest = (ServletServerHttpRequest) request;
       HttpServletRequest servletRequest = servletServerHttpRequest.getServletRequest();
-      try {
         String token = resolveToken(servletRequest, CODE).get();
-        String alarmToken = resolveToken(servletRequest, CODE_A).get();
-        Optional<MemberInfoDTO> memberInfoDTO = resolveMemberInfo(token);
-        if (memberInfoDTO.isPresent()) {
-          String key =
+      String alarmToken = resolveToken(servletRequest, CODE_A).get();
+      Optional<MemberInfoDTO> memberInfoDTO = resolveMemberInfo(token);
+      if (memberInfoDTO.isPresent()) {
+        String key =
               PREFIX_REFRESHTOKEN + memberInfoDTO.get().getId() + memberInfoDTO.get().getEmail();
-          Optional<String> refreshToken = refreshTokenDAO.getRefreshToken(aes256Util.encrypt(key));
-          if (refreshToken.isPresent()) {
-            attributes.put(TOKEN, token);
-            attributes.put(ALARM_TOKEN, alarmToken);
-            return true;
-          }
+        Optional<String> refreshToken = refreshTokenDAO.getRefreshToken(aes256Util.encrypt(key));
+        if (refreshToken.isPresent()) {
+          attributes.put(TOKEN, token);
+          attributes.put(ALARM_TOKEN, alarmToken);
+          return true;
         }
-      } catch (Exception e) {
-        e.printStackTrace();
-        return false;
       }
     }
     return false;
@@ -66,6 +67,7 @@ public class HttpHandShakeInterceptor implements HandshakeInterceptor {
       WebSocketHandler wsHandler, Exception exception) {
     // 핸드쉐이크 이후의 콜백 메서드. 일단은 처리할 것이 없다고 판단되어 비워두겠다.
     log.info("Handshake 완료");
+    log.error(exception.getMessage());
   }
 
   private Optional<String> resolveToken(HttpServletRequest servletRequest, String type) {
--- src/main/java/com/windmealchat/global/handler/StompErrorHandler.java
+++ src/main/java/com/windmealchat/global/handler/StompErrorHandler.java
@@ -15,58 +15,62 @@ import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
 @RequiredArgsConstructor
 public class StompErrorHandler extends StompSubProtocolErrorHandler {
 
-    private static final byte[] EMPTY_PAYLOAD = new byte[0];
-
-    /*
-        클라이언트 메세지 처리 도중 발생한 오류를 처리한다.
-        STOMP 프로토콜에 따르면, 서버는 이 경우 ERROR 프레임을 헤더에 달아서 전달 후 연결을 닫는데,
-        이 프레임을 전달하고 싶지 않으면 null을 반환하도록 설계하면 된다고 한다.
-        그 후 메세지 브로커를 통해서 사용자의 입맛대로 처리할 수 있다고 한다.
-     */
-    @Override
-    @Nullable
-    public Message<byte[]> handleClientMessageProcessingError(Message<byte[]> clientMessage, Throwable ex) {
-
-        log.error("예외 발생 : handleClientMessageProcessingError");
-        ex.printStackTrace();
-
-        StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.ERROR);
-        accessor.setMessage(ex.getMessage());
-        accessor.setLeaveMutable(true);
-        StompHeaderAccessor clientHeaderAccessor = null;
-        if (clientMessage != null) {
-            clientHeaderAccessor = MessageHeaderAccessor.getAccessor(clientMessage, StompHeaderAccessor.class);
-            if (clientHeaderAccessor != null) {
-                String receiptId = clientHeaderAccessor.getReceipt();
-                if (receiptId != null) {
-                    accessor.setReceiptId(receiptId);
-                }
-            }
+  /*
+      클라이언트 메세지 처리 도중 발생한 오류를 처리한다.
+      STOMP 프로토콜에 따르면, 서버는 이 경우 ERROR 프레임을 헤더에 달아서 전달 후 연결을 닫는데,
+      이 프레임을 전달하고 싶지 않으면 null을 반환하도록 설계하면 된다고 한다.
+      그 후 메세지 브로커를 통해서 사용자의 입맛대로 처리할 수 있다고 한다.
+   */
+  @Override
+  @Nullable
+  public Message<byte[]> handleClientMessageProcessingError(Message<byte[]> clientMessage,
+      Throwable ex) {
+    log.error(ex.getMessage());
+    StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.ERROR);
+    accessor.setMessage(ex.getMessage());
+    accessor.setLeaveMutable(true);
+    StompHeaderAccessor clientHeaderAccessor = null;
+    if (clientMessage != null) {
+      log.info("1 :" + new String((byte[]) clientMessage.getPayload()));
+      clientHeaderAccessor = MessageHeaderAccessor.getAccessor(clientMessage,
+          StompHeaderAccessor.class);
+      if (clientHeaderAccessor != null) {
+        log.info("2");
+        String receiptId = clientHeaderAccessor.getReceipt();
+        if (receiptId != null) {
+          log.info("3");
+          accessor.setReceiptId(receiptId);
         }
-        return handleInternal(accessor, EMPTY_PAYLOAD, ex, clientHeaderAccessor);
+      }
     }
+    byte[] payload = ex.getMessage().getBytes(java.nio.charset.StandardCharsets.UTF_8);
+    return handleInternal(accessor, payload, ex, clientHeaderAccessor);
+  }
 
-    /*
-        연결이 끊겼거나, 외부 메세지 브로커가 에러를 반환했거나, 기타 등등의 문제로 인해서
-        서버가 클라이언트에게 에러를 전달해야 할 경우 아래의 메서드가 호출된다.
-     */
-    @Override
-    @Nullable
-    public Message<byte[]> handleErrorMessageToClient(Message<byte[]> errorMessage) {
+  /*
+      연결이 끊겼거나, 외부 메세지 브로커가 에러를 반환했거나, 기타 등등의 문제로 인해서
+      서버가 클라이언트에게 에러를 전달해야 할 경우 아래의 메서드가 호출된다.
+   */
+  @Override
+  @Nullable
+  public Message<byte[]> handleErrorMessageToClient(Message<byte[]> errorMessage) {
 
-        log.error("예외발생 : handleErrorMessageToClient");
+    log.error("예외발생 : handleErrorMessageToClient");
+    log.error("페이로드 : " + new String((byte[])errorMessage.getPayload()));
 
-        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(errorMessage, StompHeaderAccessor.class);
-        Assert.notNull(accessor, "No StompHeaderAccessor");
-        if (!accessor.isMutable()) {
-            accessor = StompHeaderAccessor.wrap(errorMessage);
-        }
-        return handleInternal(accessor, errorMessage.getPayload(), null, null);
+    StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(errorMessage,
+        StompHeaderAccessor.class);
+    Assert.notNull(accessor, "No StompHeaderAccessor");
+    if (!accessor.isMutable()) {
+      accessor = StompHeaderAccessor.wrap(errorMessage);
     }
+    return handleInternal(accessor, errorMessage.getPayload(), null, null);
+  }
 
-    protected Message<byte[]> handleInternal(StompHeaderAccessor errorHeaderAccessor, byte[] errorPayload,
-                                             @Nullable Throwable cause, @Nullable StompHeaderAccessor clientHeaderAccessor) {
+  protected Message<byte[]> handleInternal(StompHeaderAccessor errorHeaderAccessor,
+      byte[] errorPayload,
+      @Nullable Throwable cause, @Nullable StompHeaderAccessor clientHeaderAccessor) {
 
-        return MessageBuilder.createMessage(errorPayload, errorHeaderAccessor.getMessageHeaders());
-    }
+    return MessageBuilder.createMessage(errorPayload, errorHeaderAccessor.getMessageHeaders());
+  }
 }
