661205077bc9fb36942fa1fff975341ffba55304
Owen-Choi <oldstyle482@gmail.com>
2024-02-05 01:19:13 +0900

    !BREAKING CHANGE : mysql 커넥션 및 JPA 의존성 추가

--- src/main/java/com/windmealchat/WindmealchatApplication.java
+++ src/main/java/com/windmealchat/WindmealchatApplication.java
@@ -2,11 +2,12 @@ package com.windmealchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
-
-@SpringBootApplication
+@EnableJpaAuditing
 @EnableMongoAuditing
+@SpringBootApplication
 public class WindmealchatApplication {
 
     public static void main(String[] args) {
